272c083e22f00dbc0c51cb62d7673104a9641035
brandaoplaster <brandaoplaster@gmail.com>
2026-04-16 20:57:59 -0300

    test: Add factory for user

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+    password { "1234567890" }
+    password_confirmation { "1234567890" }
+  end
+end
