e2285d9d34060becd0bc051394632d1520c4125b
brandaoplaster <brandaoplaster@gmail.com>
2026-04-19 11:42:41 -0300

    feat: Add has_one_attached image in product

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :active, inclusion: { in: [ true, false ] }
+
+  has_one_attached :image
 end
