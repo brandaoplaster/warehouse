7c3b504d8b803dd961b6ba07f4e5a81cc0fd5678
brandaoplaster <brandaoplaster@gmail.com>
2026-04-19 11:44:05 -0300

    feat: Adjust view and controller load products

--- app/controllers/store/home_controller.rb
+++ app/controllers/store/home_controller.rb
@@ -3,12 +3,7 @@ module Store
     require "faker"
     require "ostruct"
     def index
-      @products = 8.times.map do
-        OpenStruct.new(
-          name: Faker::Commerce.product_name,
-          price: Faker::Commerce.price(range: 10..200)
-        )
-      end
+      @products = Product.all
     end
   end
 end
