87ed7979996ef4575373d082e8e41a9f69e9c6bd
brandaoplaster <brandaoplaster@gmail.com>
2026-04-22 13:25:45 -0300

    test: Adjust orders factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     credit_card_number { Faker::Finance.credit_card(:visa).gsub('-', '') }
     credit_card_name { Faker::Name.name }
     credit_card_cvv { Faker::Number.number(digits: 3).to_s }
-    credit_card_expiration_month { rand(1..12) }
+    credit_card_expiration_month { rand(Date.current.month..12) }
     credit_card_expiration_year { rand(Date.current.year..(Date.current.year + 5)) }
   end
 end
