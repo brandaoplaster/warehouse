dfbc9749d572d98faef1cd46a5a23165ceed97a7
brandaoplaster <brandaoplaster@gmail.com>
2026-04-23 22:33:20 -0300

    feat: Adjust Autenticable in controllers

--- app/controllers/account/application_controller.rb
+++ app/controllers/account/application_controller.rb
@@ -1,5 +1,8 @@
 module Account
   class ApplicationController < ::ApplicationController
+    include Authenticable
+    require_role :client
+
     layout "dashboard"
   end
 end
--- app/controllers/store/application_controller.rb
+++ app/controllers/store/application_controller.rb
@@ -1,8 +1,5 @@
 module Store
   class ApplicationController < ::ApplicationController
-    include Authenticable
-    require_role :client
-
     layout "store"
   end
 end
