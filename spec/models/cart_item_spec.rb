694d999c4b3facbb5314e097c0f910060732f691
brandaoplaster <brandaoplaster@gmail.com>
2026-04-24 22:01:13 -0300

    style: Adjust code style

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe CartItem, type: :model do
 
   describe 'validations' do
     subject { build(:cart_item) }
-    
+
     it { should validate_presence_of(:quantity) }
     it { should validate_presence_of(:unit_price) }
     it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
