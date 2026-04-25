bc3aa9908c83e35e6fdb745d811b3e158cbea47c
brandaoplaster <brandaoplaster@gmail.com>
2026-04-24 21:40:47 -0300

    feat: Add cart and cart item migrate

---
+++ db/migrate/20260425002454_create_carts.rb
@@ -0,0 +1,11 @@
+class CreateCarts < ActiveRecord::Migration[8.0]
+  def change
+    create_table :carts do |t|
+      t.references :user, null: false, foreign_key: true
+      t.integer :status, null: false, default: 0
+      t.datetime :expired_at, null: true
+
+      t.timestamps
+    end
+  end
+end
