47fc83052d111589a407e6ebab670743c4d45555
brandaoplaster <brandaoplaster@gmail.com>
2026-04-24 21:58:29 -0300

    feat: Add validates in cart item model

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,4 +1,8 @@
 class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
+
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
+  validates :product_id, uniqueness: { scope: :cart_id, message: "has already been added to this cart" }
 end
