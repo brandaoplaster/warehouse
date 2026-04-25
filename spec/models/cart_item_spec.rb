0087218abe22ec080b3a31a06b7ce51cca1680f1
brandaoplaster <brandaoplaster@gmail.com>
2026-04-24 21:59:39 -0300

    test: Add tests for cart item model

---
+++ spec/models/cart_item_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe CartItem, type: :model do
+  describe 'associations' do
+    it { should belong_to(:cart) }
+    it { should belong_to(:product) }
+  end
+
+  describe 'validations' do
+    subject { build(:cart_item) }
+    
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+    it { should validate_numericality_of(:quantity).only_integer.is_greater_than(0) }
+    it { should validate_numericality_of(:unit_price).is_greater_than(0) }
+    it { should validate_uniqueness_of(:product_id).scoped_to(:cart_id).with_message("has already been added to this cart") }
+  end
+end
