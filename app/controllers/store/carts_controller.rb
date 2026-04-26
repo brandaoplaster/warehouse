563fa6b7fa56e3a38252f944e7e3b3eacc440cab
brandaoplaster <brandaoplaster@gmail.com>
2026-04-25 23:30:22 -0300

    feat: Add carts controller base

---
+++ app/controllers/store/carts_controller.rb
@@ -0,0 +1,16 @@
+module Store
+  class CartsController < ApplicationController
+    def show;end
+
+    def add_item
+    end
+
+    def remove_item
+    end
+
+    def update_item
+    end
+
+    privatevo7
+  end
+end
